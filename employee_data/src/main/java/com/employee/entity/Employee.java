0cae94cf69156c228f98f1be0d82d195fd2c61d0
Syndee <Cynthiaineh02@gmail.com>
2020-02-18 11:25:05 +0100

    Employee Spring MVC project

---
+++ employee_data/src/main/java/com/employee/entity/Employee.java
@@ -0,0 +1,99 @@
+package com.employee.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	@Column(name="employee_id")
+	private int employeeId;
+	
+	@Column(name="firstname")
+	private String firstname;
+	
+	@Column(name="lastname")
+	private String lastname;
+	
+	@Column(name="date_of_birth")
+	private Date dateOfBirth;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="phonenumber")
+	private String phoneNumber;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(String firstname, String lastname, Date dateOfBirth, String email, String phoneNumber) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.dateOfBirth = dateOfBirth;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+}
