20efb7329a14b449ed2c9c470304925dba17a221
Syndee <Cynthiaineh02@gmail.com>
2020-02-24 16:28:10 +0100

    Implemented Service Layer

---
+++ employeeService/src/test/java/com/employee/service/EmployeeServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.employee.service;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.employee.dao.EmployeeDao;
+import com.employee.entity.Employee;
+
+@ContextConfiguration("classpath:service-context.xml")
+@RunWith(SpringRunner.class)
+public class EmployeeServiceImplTest {
+	
+	@Mock
+	private EmployeeDao employeeDaoImpl;
+
+	@Before
+	public void setUp() throws Exception {
+		employeeDaoImpl = mock(EmployeeDao.class);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void saveEmployeeTest() {
+		
+		//create an object 
+		Employee tempEmployee = new Employee();
+		
+		//define mock method call
+		doNothing().when(employeeDaoImpl).saveEmployee(isA(Employee.class));
+		
+		// make a mock call
+		employeeDaoImpl.saveEmployee(tempEmployee);
+		
+		//verify the method was called
+		verify(employeeDaoImpl, times(1)).saveEmployee(tempEmployee);
+	}
+
+}
---
+++ employee_data/src/main/resources/db/insert-employees.sql
@@ -0,0 +1,8 @@
+use employee_db;
+
+INSERT INTO`employee_db`.`employee`(`employee_id`,`firstname`,`lastname`,`email`,`phonenumber`)
+VALUES(1,'Ben','Taylor','ben@mail.com', '09078690321'),
+(2,'Sola','Tayo','Sola@mail.com', '09078324321'),
+(3,'Chris','Samuel','Chris@mail.com', '09076754321'),
+(4,'Tola','Daniel','Tola@mail.com', '09078666321'),
+(5,'Gbemi','Matthew','Gbemi@mail.com', '09073254321');
